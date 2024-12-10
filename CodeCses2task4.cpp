d08d7b01dc85dd77b20f211acb5f8646c3320a6a
HaoLe-cyber <lehuunhathao2k6@gmail.com>
2024-12-10 09:08:26 +0700

    Code C session2 task4

---
+++ CodeCses2task4.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a,v,s;
+    printf ("hay nhap gia tri canh a: ");
+    scanf ("%d", &a);
+    v= a*4;
+    s= a*a;
+    printf ("chu vi cua hinh vuong la: %d\n ", v);
+    printf ("dien tich cua hinh vuong la: %d\n", s);
+    return 0;
+}
