17214a4dba90520ffe99319a1a8b8e4b2fb86651
HaoLe-cyber <lehuunhathao2k6@gmail.com>
2024-12-10 09:08:06 +0700

    Code C session2 task3

---
+++ CodeCses2task3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a=10;
+    int b=2;
+    int c,d,e,f;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    printf ("%d %d %d %d", c, d, e, f);
+    return 0;
+}
