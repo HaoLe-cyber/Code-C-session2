fa729bc5938dab7157396f987a423c257dc50b0c
HaoLe-cyber <lehuunhathao2k6@gmail.com>
2024-12-10 09:07:02 +0700

    Code C session2 task1

---
+++ CodeCses2task1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int number; // bien number la so nguyen
+    float real; // bien real la so thuc
+    double Num; // bien Num la so thuc
+    char character; // bien character la ky tu
+    bool name; // bien name la bien dieu kien true/false
+    return 0;
+}
